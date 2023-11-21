3ad458601e8e214cf3c5d1426ca3d928d1022540
GiovanniSCESP <giovanniandre2003@gmail.com>
2023-11-21 10:13:19 +0000

    Fix Syntax & Regex

--- README.md
+++ README.md
@@ -1,22 +1,40 @@
 # MessiScript Syntax
 
-![MessiSyntax](intro.gif)
+![MessiSyntax](img/intro.gif)
+
+Soporte para el lenguaje **[MessiScript](https://github.com/Erawaa/MessiScriptInterpreter)** en Visual Studio Code.
 
 ## Características
 - Resaltado de sintaxis.
 - Autocompletado de comandos y descripciones.
 - Soporte de comentarios `#Comentario.` o `//Comentario.`
 - Snippets de código con ejemplos `ms:snippet`.
-- Descripciones de Hover para parámetros.
+- Descripciones de hover para parámetros.
 
-### Como ejecutar MessiScript desde Visual Studio Code
-En la configuración de la extensión [Code Runner](https://marketplace.visualstudio.com/items?itemName=formulahendry.code-runner) modificamos el json **Executor Map** y añadimos la línea:
+## Instalación
+La extensión sigue en desarrollo, se puede probar copiando el repositorio a tu carpeta de extensiones de Visual Studio Code localizada en: `C:\Users\USUARIO\.vscode\extensions`.
+
+## Uso
+- #### Autocompletado
+![MessiSyntax](img/autocompletado.gif)
+![MessiSyntax](img/descripciones.png)
+
+La extensión da recomendaciones de autocompletado, así como proporcionar una descripción de cada uno de los comandos disponibles.
+
+- #### Snippets
+![MessiSyntax](img/snippets.gif)
+
+Snippets de código con ejemplos, todas las opciones de sustantivos y adjetivos.
+
+## Como ejecutar MessiScript desde Visual Studio Code
+En la configuración de la extensión **[Code Runner](https://marketplace.visualstudio.com/items?itemName=formulahendry.code-runner)** modificamos el JSON **Executor Map** y añadimos la línea:
 ```json
-"messi": "pyhon set PYTHONIOENCODING=utf8 && C:\\...\\msinterpreter.py"
+"messi": "python set PYTHONIOENCODING=utf8 && C:\\...\\msinterpreter.py"
 ```
-> Dependiendo de como esté instalado python en tu sistema puede que sea no necesario escribir python al principio o que haya que sustituirlo por 'py', 'python3' u otros.
-
+> [!TIP]
+> > Dependiendo de como esté instalado Python en su sistema, puede que sea no necesario escribir Python al principio o que haya que sustituirlo por 'py', 'python3' u otros.
 
 ### Notas
-- Tema usado: [Ayu](https://marketplace.visualstudio.com/items?itemName=teabyii.ayu)
-- Fuente: [Cascadia Code](https://github.com/microsoft/cascadia-code)
+- La extensión no dispone de una detección de errores en el código solo ayuda con el proceso de desarrollo.
+- Tema usado: **[Ayu](https://marketplace.visualstudio.com/items?itemName=teabyii.ayu)**
+- Fuente: **[Cascadia Code](https://github.com/microsoft/cascadia-code)**
--- client/extension.js
+++ client/extension.js
@@ -2,46 +2,49 @@ const vscode = require('vscode');
 const ARRAYS = require('./lists.js');
 
 
-const CommandsCompletion = []
-for (const [cmd, doc] of Object.entries(ARRAYS.COMMANDS)) {
-    var CompletionItem = new vscode.CompletionItem(cmd);
-    CompletionItem.documentation = new vscode.MarkdownString(doc);
-    CommandsCompletion.push(CompletionItem);
+// Creación de la lista de comandos para autocompletar.
+const CompletionList = []
+for ( const [cmd, doc] of Object.entries(ARRAYS.COMMANDS) ) {
+	var CompletionItem = new vscode.CompletionItem(cmd);			// Nombre del elemento list.js
+	CompletionItem.documentation = new vscode.MarkdownString(doc);	// Documentación de list.js
+	CompletionList.push(CompletionItem);
 };
 
 
 function activate(context) {
 
-    vscode.languages.registerHoverProvider('messi', {
-        provideHover(document, position, token) {
-            const range = document.getWordRangeAtPosition(position);
-            const word = document.getText(range);
+	// Hover de palabras.
+	vscode.languages.registerHoverProvider('messi', {
+		provideHover(document, position, token) {
+			const range = document.getWordRangeAtPosition(position);
+			const word = document.getText(range);
 
-            const signo = "**Fútbol**: Cambia el signo de la posición actual de la lista.";
-            const sustantivo = "**Sustantivo**: Suman uno a la posición actual de la lista.";
-            const adjetivo = "**Adjetivo**: Multiplican por dos a la posición actual de la lista." ;
-            
-            if ( word == 'fútbol' ) { var res = new vscode.Hover(signo) 
-            } else if ( ARRAYS.NOUNS.includes(word) ) { var res = new vscode.Hover(sustantivo) 
-            } else if ( ARRAYS.ADJECTIVES.includes(word) ) { var res = new vscode.Hover(adjetivo) };
-            return res;
-        }
-    });
+			const signo = "**Fútbol**: Cambia el signo de la posición actual de la lista.";
+			const sustantivo = "**Sustantivo**: Suman uno a la posición actual de la lista.";
+			const adjetivo = "**Adjetivo**: Multiplican por dos a la posición actual de la lista.";
 
-    const CompletionProvider = vscode.languages.registerCompletionItemProvider('messi', {
-        provideCompletionItems(document, position, token, context) {
-            return CommandsCompletion;
-        }
-    });
+			if (word == 'fútbol') { var res = new vscode.Hover(signo)
+			} else if (ARRAYS.NOUNS.includes(word)) { var res = new vscode.Hover(sustantivo)
+			} else if (ARRAYS.ADJECTIVES.includes(word)) { var res = new vscode.Hover(adjetivo) };
+			return res;
+		}
+	});
 
-    context.subscriptions.push(CompletionProvider);
+	// Autocompletado de elementos.
+	const CompletionProvider = vscode.languages.registerCompletionItemProvider('messi', {
+		provideCompletionItems(document, position, token, context) {
+			return CompletionList;
+		}
+	});
+
+	context.subscriptions.push(CompletionProvider);
 }
 
 
-function deactivate() { }
+function deactivate() { };
 
 
 module.exports = {
-    activate,
-    deactivate
-}
+	activate,
+	deactivate
+};
